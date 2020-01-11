9a2afad7bfc801f74c67ddad05a6dc6446bfedfc
John Doe <39686917+cybertrop@users.noreply.github.com>
2020-01-10 22:00:47 -0600

    Update vowels.py

--- vowels.py
+++ vowels.py
@@ -6,34 +6,6 @@
 # a, e, i, o, and u
 # The input string will only consist of lower case letters and/or spaces.
 
-# Iteration 1 
-# The Dirty Way Out
-
-def getCount(inputStr):
-    num_vowels = 0
-    for letter in inputStr:
-    	if letter == 'a':
-    		num_vowels = num_vowels + 1
-    	if letter == 'u':
-    		num_vowels = num_vowels + 1
-    	if letter == 'o':
-    		num_vowels = num_vowels + 1
-    	if letter == 'i':
-    		num_vowels = num_vowels + 1
-    	if letter == 'e':
-    		num_vowels = num_vowels + 1
-    	else:
-    		pass
-
-    print(num_vowels)
-    return num_vowels
-	
-	
-    
-getCount("abceioua")
-
-print()
-
 # Second iteration - more efficient and less code 
 
 def count2(inputStr):
