cb2848c3952732cca3ee55acf370957cad7af828
John Doe <39686917+cybertrop@users.noreply.github.com>
2020-01-10 21:58:25 -0600

    Update exclaim.py

--- exclaim.py
+++ exclaim.py
@@ -1,7 +1,9 @@
 #!/bin/python3
 # exclaim.py
+# Level: Easy
 
-# Remove a exclamation mark from the end of string. For a beginner kata, you can assume that the input data is always a string, no need to verify it.
+# Remove an exclamation mark from the end of string. Assume that the input data is always a string
+
 # My attempt
 def remove(s):
     if s.endswith('!'):
@@ -18,7 +20,7 @@ remove("!Hi")
 remove("Hi! Hi!")
 remove("Hi")
 
-# Other users attempts
+# Other attempts I found online
 def remove(s):
     return s[:-1] if s.endswith('!') else s
 
